804d4ac4fa1323860f321abccf35c83a9c1180a1
Kamil Arkit <KamilArkit@interia.pl>
2023-01-12 18:07:58 +0100

    To products on frontend assigned data-attributs, added event on 'add to cart' button and sending this data forward.

--- client/src/components/homePage/Home.js
+++ client/src/components/homePage/Home.js
@@ -16,7 +16,17 @@ export const Home=()=>{
         maintainProducts.getAllProducts()
         .then((response) => {
             setProducts(response.data)
-            console.log(products)
+            let productsDiv = document.getElementById("products")
+            let children = productsDiv.children;
+            
+            for(let i=0; i<response.data.length; i++){
+                children[i].setAttribute('id', response.data[i]._id)
+                children[i].children[0].dataset.productId=response.data[i]._id
+                children[i].children[1].dataset.productName=response.data[i].name
+                children[i].children[2].dataset.productCategory=response.data[i].category.name
+
+                
+            }
         })
         .catch((e)=>{
             console.log(e)
@@ -34,9 +44,12 @@ export const Home=()=>{
         })
     }
 
-    function addToCart(product){
+    function addToCart(event){
         if(userId){
-            maintainUserShoppingCart.postProduct(userId, product)
+            let products = document.getElementById("products")
+            console.log(products)
+            console.log("event data: ")
+            console.log(event.target.parentElement)
         }else{
             console.log("You are not logged in.")
         }
@@ -55,12 +68,12 @@ export const Home=()=>{
             </div>
 
             <h1>Featured Products</h1>
-            <div className='products'>
+            <div className='products' id='products'>
                 {products.map((product) => (
                 <div className='card' key={product.id}>
-                    <h3>{product.name}</h3>
-                    <h5>{product.category.name}</h5>
-                    <button id='addToCartButton' onClick={()=>addToCart()}>Add to cart</button>
+                    <h3 id="productName">{product.name}</h3>
+                    <h5 id="productCategory">{product.category.name}</h5>
+                    <button id='addToCartButton' onClick={event => addToCart(event)}>Add to cart</button>
                 </div>
             ))}
             </div>
