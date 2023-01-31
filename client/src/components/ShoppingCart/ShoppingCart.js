83739be04785010529c13029045f5bc04b146a73
Kamil Arkit <KamilArkit@interia.pl>
2023-01-31 18:14:27 +0100

    Updated view of shoppingCart component, there are displaying products of cart with amount now.

--- client/src/components/ShoppingCart/ShoppingCart.js
+++ client/src/components/ShoppingCart/ShoppingCart.js
@@ -1,46 +1,48 @@
 import React, { useEffect, useState } from "react";
 import MaintainShoppingCart from "../../services/maintainUserShoppingCart";
+import "./ShoppingCart.css";
 
 export const ShoppingCart = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     getShoppingCart();
-    //shoppingCartProducts();
   }, []);
   function getShoppingCart() {
     let customerId = localStorage.getItem("customerId");
     MaintainShoppingCart.getShoppingCart(customerId)
       .then((response) => {
         setProducts(response.data.products);
-        //groupProductsByName(response.data.products)
+        setAttributes(response.data.products);
       })
       .catch((e) => {
         console.log(e);
       });
   }
-  // var groupedProducts = {};
-  // function groupProductsByName(input) {
-  //   const products = input.products;
-    
-  //   products.forEach(product => {
-  //     const name = product.name;
-  //     if (!groupedProducts[name]) {
-  //       groupedProducts[name] = [];
-  //     }
-  //     groupedProducts[name].push(product);
-  //   });
-  //   //console.log(Object.values(groupedProducts))
-  //   return groupedProducts;
-  // }
+  async function setAttributes(products) {
+    const cartProducts = document.getElementById("cartProducts");
+    console.log(cartProducts.children)
+    for(let i=0; i<products.length; i++){
+      cartProducts.children[i].setAttribute("id", products[i]._id)
+      cartProducts.children[i].children[0].dataset=products[i].name;
+      cartProducts.children[i].children.getElementbyId("productAmount").setAttribute("data-product-amount", products[i].amountInCart);
+
+    }
+  }
+  function addOneMoreProduct() {}
 
   return (
     <>
+      <div className="cartProducts" id="cartProducts">
         {products.map((product) => (
-          <div className="categoryCard" key={product.id}>
-            <li>{product.name}</li>
+          <div key={product.id}>
+            <span>{product.name}</span>
+            <span id="decreaseAmount"> - </span>
+            <span id="productAmount">{product.amountInCart}</span>
+            <span id="increaseAmount"> + </span>
           </div>
         ))}
+      </div>
     </>
   );
 };
