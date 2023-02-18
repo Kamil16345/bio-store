731abc01c7f300b1d5ad8da5684228b37003300c
Kamil Arkit <KamilArkit@interia.pl>
2023-02-18 17:48:21 +0100

    updated gitignore

---
+++ server/functions/api.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const winston = require('winston')
+const serverless = require('serverless-http')
+const router = express.Router()
+
+require('dotenv').config();
+require('../startup/logging')()
+require('../startup/routes')(app)
+require('../startup/db')()
+require('../startup/config')
+
+var corsOptions = {
+    origin: "http://localhost:3001",
+    headers:{
+        "Access-Control-Allow-Origin":"*"
+    },
+    methods: "GET,PUT,POST,DELETE,OPTIONS"
+};
+
+router.get('/json',(req, res)=>{
+    res.json({
+        'hello':'hi'
+    })
+})
+//const port = process.env.PORT || 3000
+
+//const server = app.listen(port, ()=>console.log(`App is starting on port: ${port}`))
+
+//module.exports=server
+app.use(cors(corsOptions))
+app.use('/.netlify/functions/api', router)
+module.exports.handler = serverless(app)
